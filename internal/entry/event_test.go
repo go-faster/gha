431ab1ad4312c8938a80ba2fb4bc73a03c1904e4
Aleksandr Razumov <ernado@ya.ru>
2022-01-15 12:38:46 +0300

    test: add benchmarks

---
+++ internal/entry/event_test.go
@@ -0,0 +1,62 @@
+package entry
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"testing"
+
+	"github.com/go-faster/jx"
+)
+
+var (
+	//go:embed _testdata/2019-11-27T03.single.json
+	dataSingle []byte
+
+	//go:embed _testdata/2019-11-27T03.first100.json
+	dataMulti []byte
+)
+
+func BenchmarkEvent_Decode(b *testing.B) {
+	b.Run("Single", func(b *testing.B) {
+		d := jx.GetDecoder()
+		b.ReportAllocs()
+		b.SetBytes(int64(len(dataSingle)))
+
+		var e Event
+
+		for i := 0; i < b.N; i++ {
+			e.Reset()
+			d.ResetBytes(dataSingle)
+			if err := e.Decode(d); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+	b.Run("Multi", func(b *testing.B) {
+		d := jx.GetDecoder()
+		b.ReportAllocs()
+		b.SetBytes(int64(len(dataMulti)))
+		r := bytes.NewReader(dataMulti)
+		buf := make([]byte, 1024*1024)
+
+		for i := 0; i < b.N; i++ {
+			r.Reset(dataMulti)
+			s := bufio.NewScanner(r)
+			s.Buffer(buf, len(buf))
+
+			var e Event
+
+			for s.Scan() {
+				e.Reset()
+				d.ResetBytes(s.Bytes())
+				if err := e.Decode(d); err != nil {
+					b.Fatal()
+				}
+			}
+			if err := s.Err(); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
--- internal/entry/reader_test.go
+++ internal/entry/reader_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	_ "embed"
 	"testing"
+	"time"
 )
 
 //go:embed _testdata/2019-11-27T03.first100.json.zst
@@ -12,20 +13,32 @@ var dataCompressed []byte
 
 func BenchmarkReader_Decode(b *testing.B) {
 	var (
-		r   = NewReader()
-		br  = bytes.NewReader(dataCompressed)
 		ctx = context.Background()
+		nop = func(ctx context.Context, e *Event) error { return nil }
 	)
-	defer r.Close()
 	b.SetBytes(int64(len(dataCompressed)))
 	b.ReportAllocs()
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		br.Reset(dataCompressed)
-		if err := r.Decode(ctx, br, func(ctx context.Context, e *Event) error {
-			return nil
-		}); err != nil {
-			b.Fatal(err)
+
+	start := time.Now()
+
+	b.RunParallel(func(pb *testing.PB) {
+		var (
+			r  = NewReader()
+			br = bytes.NewReader(dataCompressed)
+		)
+		defer r.Close()
+
+		for pb.Next() {
+			br.Reset(dataCompressed)
+			if err := r.Decode(ctx, br, nop); err != nil {
+				b.Fatal(err)
+			}
 		}
-	}
+	})
+
+	duration := time.Since(start)
+	uncompressed := float64(b.N*len(dataMulti)) / duration.Seconds()
+	uncompressed /= 1024 * 1024 // MB
+	b.ReportMetric(uncompressed, "(json)MB/s")
 }
