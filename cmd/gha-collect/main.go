3e7e6e7af9c739975dfad2cf6064ef48cfa4f51b
Aleksandr Razumov <ernado@ya.ru>
2022-06-27 00:49:36 +0300

    feat(collect): write dump files

--- cmd/gha-collect/main.go
+++ cmd/gha-collect/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/go-faster/errors"
@@ -12,13 +13,31 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/go-faster/gha/internal/app"
+	"github.com/go-faster/gha/internal/entry"
 	"github.com/go-faster/gha/internal/gh"
 )
 
 type Event struct {
 	Raw       jx.Raw
 	CreatedAt time.Time
-	ID        string
+	ID        int64
+}
+
+func Append(name string, data []byte, perm os.FileMode) error {
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_APPEND, perm)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+	if _, err = f.Write(data); err != nil {
+		return err
+	}
+	if _, err := f.WriteString("\n"); err != nil {
+		return err
+	}
+	return f.Close()
 }
 
 func main() {
@@ -26,7 +45,7 @@ func main() {
 		const perPage = 100
 
 		client := gh.NewClient(http.DefaultClient, os.Getenv("GITHUB_TOKEN"))
-		latestMet := make(map[string]struct{})
+		latestMet := make(map[int64]struct{})
 		var etag string
 		for {
 			start := time.Now()
@@ -59,11 +78,15 @@ func main() {
 						}
 						return nil
 					case "id":
-						v, err := d.Str()
+						v, err := d.Num()
 						if err != nil {
 							return errors.Wrap(err, "id")
 						}
-						ev.ID = v
+						id, err := v.Int64()
+						if err != nil {
+							return err
+						}
+						ev.ID = id
 						return nil
 					default:
 						if err := d.Skip(); err != nil {
@@ -78,7 +101,7 @@ func main() {
 			}
 
 			// De-duplicating events.
-			currentMet := make(map[string]struct{})
+			currentMet := make(map[int64]struct{})
 			for _, ev := range current {
 				currentMet[ev.ID] = struct{}{}
 			}
@@ -86,6 +109,22 @@ func main() {
 			for _, ev := range current {
 				if _, ok := latestMet[ev.ID]; !ok {
 					newEvents = append(newEvents, ev)
+				}
+			}
+			sort.SliceStable(newEvents, func(i, j int) bool {
+				a, b := newEvents[i], newEvents[j]
+				// Sort ascending by created_at, id.
+				if a.CreatedAt.Equal(b.CreatedAt) {
+					return a.ID < b.ID
+				}
+				return a.CreatedAt.Before(b.CreatedAt)
+			})
+
+			// Write events to hourly json dump.
+			for _, ev := range newEvents {
+				name := ev.CreatedAt.Format(entry.Layout) + ".json"
+				if err := Append(name, ev.Raw, 0o644); err != nil {
+					return errors.Wrap(err, "failed to write event")
 				}
 			}
 
